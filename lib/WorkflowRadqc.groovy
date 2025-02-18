9131451ae2480acf516555bc6cba8d5a40cb34f5
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2025-02-18 15:21:19 +0100

    Don't require fasta param

--- lib/WorkflowRadqc.groovy
+++ lib/WorkflowRadqc.groovy
@@ -9,10 +9,8 @@ class WorkflowRadqc {
     //
     public static void initialise(params, log) {
         
-
-        if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-            System.exit(1)
+        if (params.fasta) {
+            log.info "Genome fasta file might be disregarded by this pipeline e.g. when specifying '--fasta genome.fa' or via a detectable config file."
         }
     }
 
